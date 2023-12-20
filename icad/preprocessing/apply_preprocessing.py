712c3c6213553614be5b8933b3127942fef3a07b
perecanals <perecanalscanals@gmail.com>
2023-12-20 16:28:52 +0100

    Initialized preprocessing

--- icad/preprocessing/apply_preprocessing.py
+++ icad/preprocessing/apply_preprocessing.py
@@ -24,21 +24,24 @@ for case_id in sorted([case_id for case_id in os.listdir(original_db_path)]):
     if case_id not in times_log and case_id not in errors_log:
         try:
             if not os.path.exists(os.path.join(original_db_path, case_id, f"{case_id}_ncct.nii.gz")):
-                shutil.copy(os.path.join(original_db_path, case_id, f"{case_id}_ncct_reg.nii.gz"), os.path.join(original_db_path, case_id, f"{case_id}_ncct.nii.gz"))
+                shutil.copy(os.path.join(original_db_path, case_id, f"{case_id}_ncct_reg.nii.gz"), 
+                            os.path.join(original_db_path, case_id, f"{case_id}_ncct.nii.gz"))
+            if not os.path.exists(os.path.join(original_db_path, case_id, f"{case_id}_extracranial_vessels_segmentation.nii.gz")):
+                shutil.copy(os.path.join(original_db_path, case_id, f"{case_id}_segmentation.nii.gz"), 
+                            os.path.join(original_db_path, case_id, f"{case_id}_extracranial_vessels_segmentation.nii.gz"))
             start = time()
             # Perform preprocessing
             preprocess_case(case_id=case_id,
                             atlas_path=atlas_path,
                             cta_path=os.path.join(original_db_path, case_id, f"{case_id}_cta.nii.gz"),
                             ncct_path=os.path.join(original_db_path, case_id, f"{case_id}_ncct.nii.gz"),
-                            vessel_segmentation_path=os.path.join(original_db_path, case_id, f"{case_id}_extracranial_vessel_segmentation.nii.gz"),
+                            vessel_segmentation_path=os.path.join(original_db_path, case_id, f"{case_id}_extracranial_vessels_segmentation.nii.gz"),
                             thrombus_segmentation_path=os.path.join(original_db_path, case_id, f"{case_id}_thrombus_segmentation.nii.gz"))
 
             times_log[case_id] = time() - start
+            save_json(times_log, os.path.join(base_dir, "times_preprocessing.json"))
         except Exception as e:
             errors_log[case_id] = str(e)
             print(f"Error preprocessing case {case_id}: {e}")
+            save_json(errors_log, os.path.join(base_dir, "errors_preprocessing.json"))
             continue
-        
-        save_json(times_log, os.path.join(base_dir, "times_preprocessing.json"))
-        save_json(errors_log, os.path.join(base_dir, "errors_preprocessing.json"))
